0c8298127e0746e04c0c4fe676b937fd6b733171
Maycol1414Cp <Maycol1414Cp@users.noreply.github.com>
2022-10-04 00:32:10 -0400

    grupo3-mod2 first work

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+game=Game()
+game.run()
 
-
-if __name__ == "__main__":
-     print("hello there...")
+# if __name__ == "__main__":
+#      game=Game()
+#      game.run()
+#      print("hello there...")
