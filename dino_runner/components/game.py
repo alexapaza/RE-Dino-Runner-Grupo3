b091b7599a7a217caa319b31ab6370e943d62b2e
Maycol1414Cp <Maycol1414Cp@users.noreply.github.com>
2022-10-05 14:02:00 -0400

    grupo3-mod2 obstaculo SMALL_CACTUS

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,6 +1,7 @@
 import pygame
 from dino_runner.utils.constants import BG,ICON,RUNNING,SCREEN_HEIGHT,SCREEN_WIDTH,TITLE,FPS
 from dino_runner.components.dinosaur.dinosaur import dinosaur
+from dino_runner.components.obstacles.obstaclemanager import ObstacleManager
 
 class Game:
      def __init__ (self):
@@ -16,6 +17,7 @@ class Game:
           self.x_pos_bg=0
           self.y_pos_bg=380
           self.player=dinosaur()
+          self.obstacle_manager=ObstacleManager()
 
      def run(self):
           self.playing=True
@@ -32,12 +34,14 @@ class Game:
      def updates(self):
           user_input=pygame.key.get_pressed()
           self.player.update(user_input)
+          self.obstacle_manager.update(self)
 
      def draw(self):
           self.clock.tick(FPS)
           self.screen.fill((255,255,255))
           self.draw_background()
           self.player.draw(self.screen)
+          self.obstacle_manager.draw(self.screen)
           pygame.display.update()
           pygame.display.flip()
 
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from dino_runner.components.game import Game
 game=Game()
 game.run()
-
 # if __name__ == "__main__":
 #      game=Game()
 #      game.run()
