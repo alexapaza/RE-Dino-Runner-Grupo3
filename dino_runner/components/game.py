a800920ef38f790e987b1db5a79faf3be9288d1d
ALEnsGAMER <alexgocugumo5@gmail.com>
2022-11-07 20:40:57 -0600

    Errores arreglados despues de la clase

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -4,10 +4,10 @@ from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, T
 
 class Game:
     def __init__(self) :
-        pygame.__init__()
+        pygame.init()
         pygame.display.set_caption(TITLE)
         pygame.display.set_icon(ICON)
-        self.screen = pygame.display.set_mode(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
         self.playing = False
         self.game_speed = 20
@@ -21,12 +21,13 @@ class Game:
             self.events()
             self.update()
             self.draw()
+
         pygame.quit()
 
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.playing == False
+                self.playing = False
 
     def update(self):
         self.player.update()
@@ -40,7 +41,7 @@ class Game:
         pygame.display.flip()
 
 
-    def draw_backgroud(self):
+    def draw_background(self):
         image_width = BG.get_width()
         self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg ))
         self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg ))
