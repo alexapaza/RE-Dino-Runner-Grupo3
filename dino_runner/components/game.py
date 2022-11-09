f15e4cfeb674a87021826bed728f494d3e89679c
alensgamer <alexgocugumo5@gmail.com>
2022-11-08 19:52:24 -0600

    La segunda clase

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,6 +1,8 @@
 import pygame
 from dino_runner.components.dinosour import Dinosour
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+
 
 class Game:
     def __init__(self) :
@@ -14,6 +16,7 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         self.player = Dinosour()
+        self.obstacle_manager = ObstacleManager()
 
     def run(self):
         self.playing = True
@@ -31,11 +34,13 @@ class Game:
 
     def update(self):
         self.player.update()
+        self.obstacle_manager.update(self)
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255,255,255))
         self.draw_background()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
 
         pygame.display.update()
         pygame.display.flip()
